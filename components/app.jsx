04dd261de2ae96d8eb6042f9cb39c3a0444ff64a
Oleksiy Zelenyuk <Oleksiy.Zelenyuk@symphony-solutions.eu>
2016-01-28 16:57:49 +0200

    supervise server run

--- components/app.jsx
+++ components/app.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>somorphic Application!</h1>
+        <h1>Isomorphic Application</h1>
         <div className="menu">
           <Link to='/'>Home</Link> |
           <Link to='/about'>About</Link>
